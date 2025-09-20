b920b3db732ecba449de0205cf953827c692e82d
sbobyn <50857495+sbobyn@users.noreply.github.com>
2025-09-20 11:22:44 -0700

    added basic scene demo with lights, shadow as default example

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
-import "./examples/shaderDemo";
-// import "./examples/sceneDemo";
+// A basic scene with meshes, a light, shadow,
+// and orbit controls using setupScene convenience function
+import "./examples/basicSceneDemo";
+
+// A scene with a custom shader material
+// and GUI controls using setupScene convenience function
+// import "./examples/shaderSceneDemo";
+
+// A simple fullscreen fragment shader demo using
+// the ShaderCanvas convenience class
+// import "./examples/shaderDemo";
