25271af6401bd74e08ceb0d5588784889ab77219
sbobyn <50857495+sbobyn@users.noreply.github.com>
2025-09-20 21:24:30 -0700

    fixed build config

--- template/vite.config.js
+++ template/vite.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-    plugins: [glsl()],
-    publicDir: "static/",
-    server: {
-        port: 3000,
-        host: true,
-        open: true,
-    },
-    build: {
-        outDir: "dist",
-        emptyOutDir: true,
-        sourcemap: true,
-        target: "exnext",
-    },
+  plugins: [glsl()],
+  publicDir: "static/",
+  server: {
+    port: 3000,
+    host: true,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    target: "esnext",
+  },
 });
